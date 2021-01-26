c58b53d4fb0b899ab0e5d95b9529dac30aa18393
macunan <macunan@gmail.com>
2021-01-26 18:01:09 -0300

    actualizar los archivos

--- src/App.js
+++ src/App.js
@@ -4,26 +4,34 @@ import NavBar from './components/NavBar';
 import ItemDetailContainer from './components/ItemDetailContainer.js'
 // Did at  npm install  bootstrap react-bootstrap in project base directory
  import "bootstrap/dist/css/bootstrap.min.css";
+import ItemListContainer from './components/ItemListContainer';
      // <ItemListContainer name={mensaje} />
-
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
 
 const mensaje = "Bienvenido al paraiso de las frutas secas, nosotros secamos con amor";
 function App() {
   return (
-<div name="background" class="p-3 mb-2 bg-dark text-white">
-    <div className="App">
- <NavBar/>
 
-
-
-
-         <ItemDetailContainer/>
-
+        <BrowserRouter>
+<NavBar/>
+    <Switch>
+        <Route exact path="/">
+            <ItemListContainer name="Escoger Producto a Comprar"/>
+                </Route>
+                    <Route path="/categories/:categoryid">
+                        <ItemListContainer/>
+                        </Route>
+                    <Route path="/categories">
+                        <ItemListContainer/>
+                        </Route>
+                    <Route path="/item/:itemid">
+                        <ItemDetailContainer/>
+                        </Route>
+                            </Switch>
       <header className="App-header">
       </header>
-          </div>
-      </div>
-  );
+    </BrowserRouter>
+          );
 }
 
 
